49a8563fb394e523cbd7e7013b2123eb9e867240
Gaurav Kumar <gauravkumar803109@gmail.com>
2025-12-07 09:22:54 +0530

    complete setup with clerk

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className='text-center text-xl mt-12'>About Us</h1>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+import { SignedIn, SignedOut, SignUpButton, UserButton } from '@clerk/nextjs'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-center'>
+        <nav className='p-4 border border-gray-100 w-1/2 mt-8 rounded-3xl flex justify-between items-center text-white'>
+            <div>
+                <Link href="/" className='mx-4'>Logo</Link>
+            </div>
+            <div className='flex flex-row gap-8'>
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <SignedOut>
+                    <SignUpButton mode='modal'>Get Started</SignUpButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+                
+            </div>
+            
+            
+        
+        </nav>
+    </div>
+    
+  )
+}
+
+export default Navbar
